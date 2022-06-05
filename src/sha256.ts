85f1cd153af53c4584630ec29fa4732fd0ed3165
Carl Duranleau <carlduranleau@gmail.com>
2022-06-05 13:09:41 -0400

    Fixed sha256 imports and rollup config

--- src/sha256.ts
+++ src/sha256.ts
@@ -20,26 +20,26 @@
  * SOFTWARE.
  */
 
-import createHash  from 'create-hash';
+import CryptoJS from 'crypto';
 export class SHA256 {
 
     public static hashTwice(buffer: Buffer): Buffer{
-        let firstHash = createHash('sha256').update(buffer).digest();
-        return createHash('sha256').update(firstHash).digest()
+        let firstHash = CryptoJS.createHash('sha256').update(buffer).digest();
+        return CryptoJS.createHash('sha256').update(firstHash).digest()
     }
 
     public static sha256ripemd160(buffer: Buffer): Buffer{
-        let firstHash = createHash('sha256').update(buffer).digest();
-        return createHash('ripemd160').update(firstHash).digest()
+        let firstHash = CryptoJS.createHash('sha256').update(buffer).digest();
+        return CryptoJS.createHash('ripemd160').update(firstHash).digest()
     }
 
     public static encodeToString(...inputs: Buffer[]): string {
         let fullInput = inputs.reduce((acc, curr) => Buffer.concat([acc, curr]), Buffer.from(""));
-        return createHash("sha256").update(fullInput).digest().toString();
+        return CryptoJS.createHash("sha256").update(fullInput).digest().toString();
     }
 
     public static encodeToBuffer(...inputs: Buffer[]): Buffer {
         let fullInput = inputs.reduce((acc, curr) => Buffer.concat([acc, curr]), Buffer.from(""));
-        return createHash("sha256").update(fullInput).digest();
+        return CryptoJS.createHash("sha256").update(fullInput).digest();
     }
 }
